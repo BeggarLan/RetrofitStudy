ad639cb5b5e75a33cb9990f67f5312250616cfc2
BeggarLan <1348270542@qq.com>
2022-04-22 23:32:08 +0800

    ServicemETHOD

---
+++ lretrofit/src/main/java/com/example/lretrofit/ServiceMethod.java
@@ -0,0 +1,28 @@
+package com.example.lretrofit;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+/**
+ * author: BeggarLan
+ * created on: 2022/4/22 22:27
+ * description:
+ */
+public class ServiceMethod {
+
+    static ServiceMethod parseAnnotation(@NonNull LRetrofit retrofit, @NonNull Method method) {
+
+        // 返回类型
+        Type returnType = method.getGenericReturnType();
+        if(Utils.haUnresolvableType(returnType)) {
+            throw new IllegalArgumentException("class:"+method.getDeclaringClass().getName()+", method:" + method+ " ,returnType error: " +returnType);
+        }
+        if(returnType == Void.TYPE) {
+            throw new IllegalArgumentException("class:"+method.getDeclaringClass().getName()+", method:" + method+ " cannot return void");
+        }
+        return
+    }
+
+}
