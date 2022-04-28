055aee340c7d0fc597e1b074e8ac3ee17bc35abc
BeggarLan <1348270542@qq.com>
2022-04-28 10:20:44 +0800

    增加Field处理类

--- lretrofit/src/main/java/com/example/lretrofit/ParameterHandler.java
+++ lretrofit/src/main/java/com/example/lretrofit/ParameterHandler.java
@@ -1,5 +1,7 @@
 package com.example.lretrofit;
 
+import androidx.annotation.Nullable;
+
 /**
  * author: lanweihua
  * created on: 2022/4/24 12:47 下午
@@ -7,6 +9,27 @@ package com.example.lretrofit;
  */
 public abstract class ParameterHandler<T> {
 
+  abstract void apply(RequestBuilder requestBuilder, @Nullable T value);
 
+  static final class Field<T> extends ParameterHandler<T> {
+
+    // 字段名
+    private final String mName;
+    // 转换器
+    private final Converter<T, String> mConverter;
+    // 是否编码
+    private final boolean mEncoded;
+
+    public Field(String name, Converter<T, String> converter, boolean encoded) {
+      mName = name;
+      mConverter = converter;
+      mEncoded = encoded;
+    }
+
+    @Override
+    void apply(RequestBuilder requestBuilder, @Nullable T value) {
+
+    }
+  }
 
 }
